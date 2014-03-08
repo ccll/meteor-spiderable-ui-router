0f90963a381445882bc1ad28dc3b9da875414337
ccll <cailei@live.com>
2014-03-08 15:11:17 +0800

    'weak' reference is buggy, use manual check instead.

--- package.js
+++ package.js
@@ -2,17 +2,26 @@ Package.describe({
   summary: "Makes the angular ui-router application crawlable to web spiders"
 });
 
+function packageExists(pkgname) {
+    var fs = Npm.require('fs');
+    var path = Npm.require('path');
+    var pkgpath = path.join('packages', pkgname);
+    return fs.existsSync(pkgpath);
+}
+
 Package.on_use(function (api) {
-  api.use('webapp', 'server');
-  api.use(['templating'], 'client');
-  api.use(['underscore'], ['client', 'server']);
+    api.use('webapp', 'server');
+    api.use(['templating'], 'client');
+    api.use(['underscore'], ['client', 'server']);
 
-  api.use('angular-ui-router', 'client', {weak: true});
+    if (packageExists('angular-ui-router')) {
+        api.use('angular-ui-router', 'client');
+    }
 
-  api.export('Spiderable', ['client', 'server']);
+    api.export('Spiderable', ['client', 'server']);
 
-  api.add_files('spiderable.html', 'client');
-  api.add_files('client.js', 'client');
+    api.add_files('spiderable.html', 'client');
+    api.add_files('client.js', 'client');
 
-  api.add_files('spiderable.js', 'server');
+    api.add_files('spiderable.js', 'server');
 });
